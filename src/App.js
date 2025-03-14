8cf5919faf46049c8a951d36958304e5edb5b26f
Maria S <stanmaria9327@yahoo.com>
2025-03-14 17:40:47 +0200

    Fix routing and warnings for React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
@@ -12,16 +12,16 @@ import attractionsAndHotels from './components/attractionsAndHotels';
 function App() {
   console.log(attractionsAndHotels); 
   return (
-    <BrowserRouter basename="/">
+    <Router basename="/">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/about" element={<About />} />
         <Route path="/location/:cityName" element={<LocationPage />} />
-        <Route path="/hotel/:hotelId" element={<HotelPage/>} />
+        <Route path="/hotel/:hotelId" element={<HotelPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
