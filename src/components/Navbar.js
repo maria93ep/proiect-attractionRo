84a2237d33efc07fdd2722235e586f554a50c2d7
Maria S <stanmaria9327@yahoo.com>
2025-03-14 19:47:57 +0200

    Made navbar responsive for smaller screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,4 +36,19 @@ const Navbar = () => {
           <span style={{ color: 'yellow', fontWeight: 'bold', fontStyle: 'italic', marginRight: '7px' }}>Destinations</span>
           <span style={{ color: 'red', fontWeight: 'bold', fontStyle: 'italic' }}>to Explore</span>
           <img 
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Dispunerea_provinciilor_istorice_
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Dispunerea_provinciilor_istorice_pe_harta_Rom%C3%A2niei.jpg/2560px-Dispunerea_provinciilor_istorice_pe_harta_Rom%C3%A2niei.jpg" 
+            alt="Icon" 
+            style={{ height: '60px', marginLeft: '10px' }} 
+          />
+        </span>
+      </div>
+      <div className="navbar-nav">
+        <Link to="/blog" className="nav-link" style={{ color: 'white', marginRight: '10px' }}>Blog</Link>
+        <Link to="/about" className="nav-link" style={{ color: 'white', marginRight: '10px' }}>About</Link>
+        <a href="https://www.vola.ro/bilete-avion/" className="nav-link btn" style={{ whiteSpace: 'nowrap' }}>Book Your Flight</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
